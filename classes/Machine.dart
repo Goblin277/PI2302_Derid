10bb59fc147f979df5475a9b15cb6408c22f676a
Goblin277 <maksderid69@yandex.ru>
2026-03-25 14:50:08 +0300

    добавил геттеры и сеттеры, сделал переменные класса приватными

--- classes/Machine.dart
+++ classes/Machine.dart
@@ -1,8 +1,18 @@
 class Machine {
-  int coffeeBeans;
-  int milk;
-  int water;
-  int cash;
+  int _coffeeBeans;
+  int _milk;
+  int _water;
+  int _cash;
 
-  Machine(this.coffeeBeans, this.milk, this.water, this.cash);
+  int get coffeeBeans => _coffeeBeans;
+  int get milk => _milk;
+  int get water => _water;
+  int get cash => _cash;
+
+  set coffeeBeans(int value) => _coffeeBeans = value;
+  set milk(int value) => _milk = value;
+  set water(int value) => _water = value;
+  set cash(int value) => _cash = value;
+
+  Machine(this._coffeeBeans, this._milk, this._water, this._cash);
 }
